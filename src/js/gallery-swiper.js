6e4a636d17a34bbdb55a37db69575fee7b9acb75
Bilogolovyi Sergii <sergey.hotchili@gmail.com>
2022-09-17 22:49:11 +0800

    added swiper and few stiles, download pictures for background

---
+++ src/js/gallery-swiper.js
@@ -0,0 +1,23 @@
+new Swiper('.image-slider',{
+    // pagination: {
+    //     el: '.swiper-pagination',
+    // },
+    //         navigation: {
+    //         nextEl: '.swiper-button-next',
+    //         prevEl: '.swiper-button-prev',
+    // },
+    // scrollbar: {
+    //     el: '.swiper-scrollbar',
+    //     draggable: true
+    // },
+    // simulateTouch: true,
+    // touchRatio: 2,
+    // grabCurser: true,
+    mousewheel:{
+        sensitivity:1,
+    },
+    loop: true,
+    loopedSlides: 1,
+
+    spaceBetween: 50,
+});
