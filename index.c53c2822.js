4796352bafb6369145196bcd74a4c88a780e58b2
Dmytro-Pinchuk <109178754+Dmytro-Pinchuk@users.noreply.github.com>
2022-09-18 15:42:04 +0000

    Deploying to gh-pages from @ Dmytro-Pinchuk/gr12-ice-cream-project@c737293a9c4e5b30cf972efac2de0be8a0419415 🚀

--- index.c53c2822.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".mySwiper",{pagination:{el:".swiper-pagination",clickable:!0}});
-//# sourceMappingURL=index.c53c2822.js.map
--- index.f7cc77eb.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".image-slider",{loop:!0,loopedSlides:1,keyboard:{enabled:!0,onlyInViewport:!0},spaceBetween:40,slideToClickedSlide:!0});
-//# sourceMappingURL=index.f7cc77eb.js.map
